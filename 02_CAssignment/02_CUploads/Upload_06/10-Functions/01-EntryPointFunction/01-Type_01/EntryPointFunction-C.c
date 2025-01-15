f167fc17b9488bc23a2e9d593349222b26ae4dd1
gauravyolsp <gauravyolsp47@gmail.com>
2025-01-16 00:42:13 +0530

    changes

--- 02_CAssignment/02_CUploads/Upload_06/10-Functions/01-EntryPointFunction/01-Type_01/EntryPointFunction-C.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> // 'stdio.h' contains declaration of 'printf()'
-
-// Entery-pont function => main() => No return type (void) and No Parameters (void), Hence No Command-Line Arguments
-
-void main(void)
-{
-	//code
-	printf("Hello World !!!\n"); //Library Function
-}
