2bac33826e04f7ebc3486c8b78d61b31f5ae79f1
gauravyolsp <gauravyolsp47@gmail.com>
2025-07-28 23:22:18 +0530

    changing folder structure

--- 02_OpenGL/01_FPP/01_Window/09_GameLoop/build.bat
+++
@@ -1,3 +0,0 @@
-cl.exe /c /EHsc window.c
-rc.exe window.rc
-link.exe window.obj window.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS
--- 02_OpenGL/01_FPP/02_OpenGL/33_UtahTeaport/OGL.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define MYICON 101
-#define IDBITMAP_MARBLE 102
